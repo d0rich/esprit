7de6043c91475cf989eeb3cb41e9ad38ce43e1ac
Nikolay Dorofeev <dorich2000@gmail.com>
2023-11-05 17:08:15 +0400

    Delete nuxt content mermaid playground

--- apps/nuxt-content-mermaid-playground/app.config.ts
+++
@@ -1,5 +0,0 @@
-export default defineAppConfig({
-  contentMermaid: {
-    color: '@nuxtjs/color-mode'
-  }
-})
--- apps/nuxt-content-mermaid-playground/nuxt.config.ts
+++
@@ -1,8 +0,0 @@
-export default defineNuxtConfig({
-  extends: ['../'],
-  modules: ['@nuxtjs/color-mode'],
-  colorMode: {
-    classSuffix: '',
-    preference: 'dark'
-  }
-})
