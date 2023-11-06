13a290f5d96b395e3367b87d9c7fe6709747b463
Nikolay Dorofeev <dorich2000@gmail.com>
2023-11-06 11:35:29 +0400

    Update ignore build scripts

---
+++ apps/d0xigen.d0rich.me/ignore-build.mjs
@@ -0,0 +1,14 @@
+import { execSync } from 'child_process'
+
+const cachedCommitRef = process.env.CACHED_COMMIT_REF || 'd06423d365fa9cf3ef99a8653abbf1a83fe7d3bb'
+const commitRef = process.env.COMMIT_REF || 'HEAD'
+
+const changesList = execSync(`git diff --name-only ${cachedCommitRef} ${commitRef}`).toString()
+
+const isD0xigenD0richMeChanged = changesList.includes('apps/d0xigen.d0rich.me')
+const isNuxtContentMermaidChanged = changesList.includes('packages/nuxt-content-mermaid')
+const isNuxtDesignSystemChanged = changesList.includes('packages/nuxt-design-system')
+
+const ignoreBuild = !isD0xigenD0richMeChanged && !isNuxtContentMermaidChanged && !isNuxtDesignSystemChanged
+
+process.exit(ignoreBuild ? 0 : 1)
