3f3c198ae4d12d7bc1a30547cef4f4d2a4e4b5e1
Nikolay Dorofeev <dorich2000@gmail.com>
2023-11-07 14:42:24 +0400

    Configure lib build

--- packages/esprit-design/vite.config.ts
+++ packages/esprit-design/vite.config.ts
@@ -1,7 +1,29 @@
+import { resolve } from "path";
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    lib: {
+      // src/indext.ts is where we have exported the component(s)
+      entry: resolve(__dirname, "src/index.ts"),
+      name: "EspritDesignLibrary",
+      // the name of the output files when the build is run
+      fileName: "esprit-design-lib",
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ["vue"],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          vue: "Vue",
+        },
+      },
+    },
+  },
 })
