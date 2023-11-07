631a08eff7bb00466dd24866a0566190c8142a08
Nikolay Dorofeev <dorich2000@gmail.com>
2023-11-07 14:51:20 +0400

    Copy wrap components

--- packages/esprit-design/src/components/utils/DWrapFocusHighlight.vue
+++ packages/esprit-design/src/components/utils/DWrapFocusHighlight.vue
@@ -1,4 +1,6 @@
 <script lang="ts">
+import { computed } from 'vue';
+
 export default {
   name: 'DWrapFocusHighlight'
 }
---
+++ packages/esprit-design/src/components/utils/index.ts
@@ -0,0 +1,3 @@
+export * from './DWrapFocusHighlight.vue'
+export * from './DWrapBackground.vue'
+export * from './DWrapShape.vue'
