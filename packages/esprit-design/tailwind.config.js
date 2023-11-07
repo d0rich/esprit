427c4e82189e7516a31676dc12988b5da5d35b8a
Nikolay Dorofeev <dorich2000@gmail.com>
2023-11-07 16:24:09 +0400

    Tailwind dependency

--- packages/esprit-design/tailwind.config.js
+++ packages/esprit-design/tailwind.config.js
@@ -21,7 +21,6 @@ module.exports = {
     }
   },
   plugins: [
-    require('@tailwindcss/typography'),
     require('./plugins/shadows')
   ]
 }
--- packages/nuxt-design-system/tailwind.config.js
+++ packages/nuxt-design-system/tailwind.config.js
@@ -3,24 +3,9 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: 'class',
-  content: [],
-  theme: {
-    fontFamily: {
-      sans: ['Roboto', ...defaultTheme.fontFamily.sans],
-      serif: ['"Roboto Slab"', ...defaultTheme.fontFamily.sans],
-      mono: ['"JetBrains Mono"', ...defaultTheme.fontFamily.mono],
-      dialog: ['Arsenal']
-    },
-    extend: {
-      screens: {
-        print: { raw: 'print' }
-      }
-    }
-  },
+  presets: [require('@d0rich/esprit-design/tailwind.config')],
   plugins: [
     require('@tailwindcss/typography'),
-    require('./plugins/tailwind/shadows'),
     require('./plugins/tailwind/nuxt-typography')
   ]
 }
