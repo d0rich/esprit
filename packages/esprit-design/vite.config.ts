f235e47888dd841eb7a8bdc7545f4e2409a33553
Nikolay Dorofeev <dorich2000@gmail.com>
2023-11-07 20:47:27 +0400

    Generate types on build

--- packages/esprit-design/vite.config.ts
+++ packages/esprit-design/vite.config.ts
@@ -1,17 +1,23 @@
 import { resolve } from 'path'
-import { defineConfig } from 'vite'
+import { defineConfig, splitVendorChunkPlugin } from 'vite'
+import dts from "vite-plugin-dts";
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    splitVendorChunkPlugin(),
+    dts({
+      insertTypesEntry: true
+    })
+  ],
   build: {
     lib: {
       // src/indext.ts is where we have exported the component(s)
-      entry: resolve(__dirname, 'src/index.ts'),
-      name: 'EspritDesignLibrary',
-      // the name of the output files when the build is run
-      fileName: 'esprit-design-lib'
+      entry: resolve(__dirname, 'src'),
+      name: 'EspritDesign',
+      fileName: 'esprit-design'
     },
     rollupOptions: {
       // make sure to externalize deps that shouldn't be bundled
