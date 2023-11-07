14c1f15001a4f988c43f1b6151c28ed16f855a48
Nikolay Dorofeev <dorich2000@gmail.com>
2023-11-07 16:07:55 +0400

    Copy polygon animations utils

--- packages/nuxt-design-system/composables/utils.ts
+++ packages/nuxt-design-system/composables/utils.ts
@@ -1,18 +1,3 @@
-export const usePolygonAnimationUtils = () => ({
-  generatePolygonLineKeyframes,
-  generatePolygonPointsKeyframes,
-  applyLinePerPointAnimation,
-  applyStaticPoints,
-  createCoordsFromArray,
-  createLineEdgeFromArray,
-  createLineFromArray
-})
-
-export const useElementsUtils = () => ({
-  getAbsoluteBoundingsGetters,
-  getAbsoluteBoundings
-})
-
 export const useDPaginationUtils = () => ({
   getLinkToPaginatedPage
 })
