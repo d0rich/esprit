e49f078787c41c54e364ea1c323f00dfa87fa147
Nikolay Dorofeev <dorich2000@gmail.com>
2023-11-07 16:00:06 +0400

    Make quote 2 directional

--- packages/esprit-design/src/components/content/DQuote.vue
+++ packages/esprit-design/src/components/content/DQuote.vue
@@ -1,11 +1,22 @@
 <script setup lang="ts">
 import DWrapShape from '../utils/DWrapShape.vue'
+
+defineProps({
+  direction: {
+    type: String as () => 'left' | 'right',
+    default: 'right'
+  }
+})
 </script>
 
 <template>
   <DWrapShape
     tag="blockquote"
     class="d-blockquote"
+    :class="{
+      'd-blockquote--left': direction === 'left',
+      'd-blockquote--right': direction === 'right'
+    }"
     shape-class="d-blockquote__shape"
   >
     <template #bg-overlay>
@@ -22,17 +33,39 @@ import DWrapShape from '../utils/DWrapShape.vue'
   @apply font-dialog text-black dark:text-white my-6;
 }
 
-.d-blockquote__body {
-  padding: var(--shape-bubble--right__padding);
-}
-
 .d-blockquote__outline {
   @apply absolute inset-0 bg-black dark:bg-white;
-  clip-path: var(--shape-bubble--right__outline);
 }
 
 .d-blockquote__shape {
   @apply bg-white dark:bg-black opacity-80;
+}
+
+/* Right */
+
+.d-blockquote--right .d-blockquote__body {
+  padding: var(--shape-bubble--right__padding);
+}
+
+.d-blockquote--right .d-blockquote__outline {
+  clip-path: var(--shape-bubble--right__outline);
+}
+
+.d-blockquote--right .d-blockquote__shape {
   clip-path: var(--shape-bubble--right);
+}
+
+/* Left */
+
+.d-blockquote--left .d-blockquote__body {
+  padding: var(--shape-bubble--left__padding);
+}
+
+.d-blockquote--left .d-blockquote__outline {
+  clip-path: var(--shape-bubble--left__outline);
+}
+
+.d-blockquote--right .d-blockquote__shape {
+  clip-path: var(--shape-bubble--left);
 }
 </style>
