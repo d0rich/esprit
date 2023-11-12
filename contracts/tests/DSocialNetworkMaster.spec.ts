b6675ff720d31152117e89146cb319a6fc505762
Nikolay Dorofeev <dorich2000@gmail.com>
2023-11-12 09:45:32 +0400

    Leave only one test

--- contracts/tests/DSocialNetworkMaster.spec.ts
+++ contracts/tests/DSocialNetworkMaster.spec.ts
@@ -1,43 +1,42 @@
-// TODO: fix tests
-// import { Blockchain, SandboxContract } from '@ton-community/sandbox'
-// import { toNano } from 'ton-core'
-// import { DSocialNetworkMaster } from '../wrappers/DSocialNetworkMaster'
-// import '@ton-community/test-utils'
+import { Blockchain, SandboxContract } from '@ton-community/sandbox'
+import { toNano } from 'ton-core'
+import { DSocialNetworkMaster } from '../wrappers/DSocialNetworkMaster'
+import '@ton-community/test-utils'
 
-// describe('DSocialNetworkMaster', () => {
-//   let blockchain: Blockchain
-//   let dSocialNetworkMaster: SandboxContract<DSocialNetworkMaster>
+describe('DSocialNetworkMaster', () => {
+  let blockchain: Blockchain
+  let dSocialNetworkMaster: SandboxContract<DSocialNetworkMaster>
 
-//   beforeEach(async () => {
-//     blockchain = await Blockchain.create()
+  beforeEach(async () => {
+    blockchain = await Blockchain.create()
 
-//     dSocialNetworkMaster = blockchain.openContract(
-//       await DSocialNetworkMaster.fromInit()
-//     )
+    dSocialNetworkMaster = blockchain.openContract(
+      await DSocialNetworkMaster.fromInit()
+    )
 
-//     const deployer = await blockchain.treasury('deployer')
+    const deployer = await blockchain.treasury('deployer')
 
-//     const deployResult = await dSocialNetworkMaster.send(
-//       deployer.getSender(),
-//       {
-//         value: toNano('0.05')
-//       },
-//       {
-//         $$type: 'Deploy',
-//         queryId: 0n
-//       }
-//     )
+    const deployResult = await dSocialNetworkMaster.send(
+      deployer.getSender(),
+      {
+        value: toNano('0.05')
+      },
+      {
+        $$type: 'Deploy',
+        queryId: 0n
+      }
+    )
 
-//     expect(deployResult.transactions).toHaveTransaction({
-//       from: deployer.address,
-//       to: dSocialNetworkMaster.address,
-//       deploy: true,
-//       success: true
-//     })
-//   })
+    expect(deployResult.transactions).toHaveTransaction({
+      from: deployer.address,
+      to: dSocialNetworkMaster.address,
+      deploy: true,
+      success: true
+    })
+  })
 
-//   it('should deploy', async () => {
-//     // the check is done inside beforeEach
-//     // blockchain and dSocialNetworkMaster are ready to use
-//   })
-// })
+  it('should deploy', async () => {
+    // the check is done inside beforeEach
+    // blockchain and dSocialNetworkMaster are ready to use
+  })
+})
