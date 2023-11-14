4d9029057b96d491c76a0c8773636803fa3cfbd0
Nikolay Dorofeev <dorich2000@gmail.com>
2023-11-14 19:43:03 +0400

    Deploy test post to test how it looks like on getgems

---
+++ contracts/scripts/deployTestSocialNetwork.ts
@@ -0,0 +1,73 @@
+import { toNano, Dictionary } from 'ton-core'
+import { NetworkProvider } from '@ton-community/blueprint'
+import { DSocialNetworkMaster } from '../wrappers/DSocialNetworkMaster'
+import {
+  DSocialNetworkAccount,
+  NftMetadataAttribute
+} from '../wrappers/DSocialNetworkAccount'
+import { DSocialNetworkPost } from '../wrappers/DSocialNetworkPost'
+
+export async function run(provider: NetworkProvider) {
+  const dMaster = provider.open(await DSocialNetworkMaster.fromInit())
+
+  await dMaster.send(
+    provider.sender(),
+    {
+      value: toNano('0.05')
+    },
+    {
+      $$type: 'Deploy',
+      queryId: 0n
+    }
+  )
+
+  await provider.waitForDeploy(dMaster.address)
+
+  await dMaster.send(
+    provider.sender(),
+    { value: toNano('0.5') },
+    {
+      $$type: 'RegisterAccount',
+      query_id: 0n,
+      account_name: 'test',
+      account_description: 'Test account description'
+    }
+  )
+
+  const accountAddress = await dMaster.getGetAccountAddressByIndex(0n)
+
+  const dAccount = provider.open(
+    DSocialNetworkAccount.fromAddress(accountAddress!)
+  )
+
+  await provider.waitForDeploy(dAccount.address)
+
+  const attributes = Dictionary.empty<bigint, NftMetadataAttribute>()
+  attributes.set(0n, {
+    $$type: 'NftMetadataAttribute',
+    trait_type: 'content',
+    value: 'This is my first post'
+  })
+
+  await dAccount.send(
+    provider.sender(),
+    { value: toNano('0.5') },
+    {
+      $$type: 'MintNft',
+      query_id: 0n,
+      individual_content: {
+        $$type: 'NftMetadata',
+        name: 'Test post',
+        description: 'Test post description',
+        image: 'https://test.com/image.png',
+        content_url: 'https://test.com/content.txt',
+        attributes
+      }
+    }
+  )
+
+  const postAddress = await dAccount.getGetNftAddressByIndex(0n)
+
+  const dPost = provider.open(DSocialNetworkPost.fromAddress(postAddress!))
+  await provider.waitForDeploy(dPost.address)
+}
