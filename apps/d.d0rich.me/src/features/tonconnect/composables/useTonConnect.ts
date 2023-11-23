dcbe5992f73da29420dacd850da8da5131625013
Nikolay Dorofeev <dorich2000@gmail.com>
2023-11-23 13:04:24 +0400

    Add wallet functionality to useTonConnect

--- apps/d.d0rich.me/src/features/tonconnect/composables/useTonConnect.ts
+++ apps/d.d0rich.me/src/features/tonconnect/composables/useTonConnect.ts
@@ -1,4 +1,7 @@
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
+import type { SenderArguments } from 'ton-core'
+import { Address } from 'ton-core/dist/address/Address'
+import { CHAIN } from '@tonconnect/protocol'
 import { TonConnectUI } from '@tonconnect/ui'
 import { useAppConfig } from '@/shared/composables/useAppConfig'
 
@@ -14,6 +17,60 @@ const tonconnect = ref<TonConnectUI>(
   })
 )
 
+const wallet = ref(tonconnect.value.wallet)
+const cancelWalletSubscription = tonconnect.value.onStatusChange(
+  (status) => (wallet.value = status)
+)
+
+export function useTonConnect() {
+  return {
+    tonconnect: tonconnect.value,
+    isConnected: computed(() => !!wallet.value?.account.address),
+    wallet: computed(() => wallet.value?.account.address ?? null),
+    network: computed(() => {
+      const chain = wallet.value?.account.chain
+      if (chain === CHAIN.MAINNET) return 'MAINNET'
+      if (chain === CHAIN.TESTNET) return 'TESTNET'
+      return null
+    }),
+    sender: {
+      send: sendTransaction,
+      address: computed(() => {
+        const addressString = wallet.value?.account.address
+        if (!addressString) return null
+        return Address.parse(addressString)
+      })
+    },
+    setOptions,
+    setRenderRoot,
+    cleanupSubscriptions: cleanup
+  }
+}
+
+function sendTransaction(args: SenderArguments) {
+  return tonconnect.value.sendTransaction({
+    messages: [
+      {
+        address: args.to.toString(),
+        amount: args.value.toString(),
+        payload: args.body?.toBoc().toString('base64')
+      }
+    ],
+    validUntil: Date.now() + 5 * 60 * 1000 // 5 minutes for user to approve
+  })
+}
+
+/**
+ * Cancel all subscriptions for TonConnectUI
+ */
+function cleanup() {
+  cancelWalletSubscription()
+}
+
+/**
+ * Define styles for the TonConnectUI button
+ * @param options UI options of TonConnectUI
+ */
 function setOptions(
   options: Omit<InstanceType<typeof TonConnectUI>['uiOptions'], 'buttonRootId'>
 ) {
@@ -23,17 +80,13 @@ function setOptions(
   }
 }
 
+/**
+ * Set buttonRootId element and makes button render in it
+ * @param rootId id of the element where the button will be rendered
+ */
 function setRenderRoot(rootId: string | null) {
   tonconnect.value.uiOptions = {
     ...tonconnect.value.uiOptions,
     buttonRootId: rootId
   }
-}
-
-/**
- * Initialize TonConnect UI only in component, which is always rendered.
- *
- */
-export function useTonConnect() {
-  return { tonconnect: tonconnect.value, setOptions, setRenderRoot }
 }
