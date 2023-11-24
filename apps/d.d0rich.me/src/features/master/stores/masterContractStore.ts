3abf53a7b39dcc9cfcd3c90892e64881ef65f106
Nikolay Dorofeev <dorich2000@gmail.com>
2023-11-24 11:42:20 +0400

    Master contract store

---
+++ apps/d.d0rich.me/src/features/master/stores/masterContractStore.ts
@@ -0,0 +1,23 @@
+import { defineStore } from 'pinia'
+import { ref } from 'vue'
+import { Address } from 'ton-core/dist/address/Address'
+import { DSocialNetworkMaster } from '@d0rich/contracts/wrappers/DSocialNetworkMaster'
+
+const masterContractAddress = 'kQBzW8ZRV2T-rx3xfAwIZyiO2FN4wCpI7If4Exj7vX9DElJG'
+
+export const useMasterContractStore = defineStore('master-contract', () => {
+  const masterContract = masterContractFromAddress(masterContractAddress)
+  const userFriendlyAddress = ref(masterContract.address.toString())
+
+  return {
+    masterContract,
+    userFriendlyAddress
+  }
+})
+
+function masterContractFromAddress(address: string | Address) {
+  if (typeof address === 'string') {
+    address = Address.parse(address)
+  }
+  return DSocialNetworkMaster.fromAddress(address)
+}
