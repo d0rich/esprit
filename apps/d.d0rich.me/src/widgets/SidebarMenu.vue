583bd7dce3bd73688e5e9ebd76b9a261f2f996e2
Nikolay Dorofeev <dorich2000@gmail.com>
2023-11-25 16:35:05 +0400

    Init sidebar navigation

--- apps/d.d0rich.me/src/widgets/SidebarMenu.vue
+++ apps/d.d0rich.me/src/widgets/SidebarMenu.vue
@@ -1,9 +1,21 @@
 <script setup lang="ts">
+import { ref } from 'vue'
+import { DBtn } from '@d0rich/esprit-design'
 import TonConnect from '../features/tonconnect/ui/TonConnect.vue'
 import UserAvatar from '../features/user/ui/UserAvatar.vue'
 import { useTonConnectStore } from '@/features/tonconnect/stores/tonConnectStore'
 
 const tonConnectStore = useTonConnectStore()
+const navLinks = ref([
+  {
+    title: 'Home',
+    path: '/'
+  },
+  {
+    title: 'Settings',
+    path: '/settings'
+  }
+])
 </script>
 
 <template>
@@ -16,5 +28,25 @@ const tonConnectStore = useTonConnectStore()
       />
       <TonConnect />
     </section>
+    <nav class="my-5">
+      <ul>
+        <li
+          v-for="link in navLinks"
+          :key="link.path"
+          class="items-start py-1 transition-colors"
+        >
+          <DBtn
+            :to="link.path"
+            highlight="composite-list-item"
+            color-class="bg-neutral-300 dark:bg-black"
+            no-rotate
+            text-transform="none"
+            class="!font-medium"
+          >
+            {{ link.title }}
+          </DBtn>
+        </li>
+      </ul>
+    </nav>
   </div>
 </template>
