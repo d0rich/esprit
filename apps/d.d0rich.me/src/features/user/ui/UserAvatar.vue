a70e00fc74d74152bddf08db7aa2ccb084698c0d
Nikolay Dorofeev <dorich2000@gmail.com>
2023-11-25 16:15:13 +0400

    Generate user avatar by wallet

---
+++ apps/d.d0rich.me/src/features/user/ui/UserAvatar.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+import { createAvatar } from '@dicebear/core'
+import { adventurerNeutral } from '@dicebear/collection'
+
+const props = defineProps({
+  wallet: {
+    type: String,
+    required: true
+  }
+})
+
+const avatar = computed(() => {
+  return createAvatar(adventurerNeutral, {
+    seed: props.wallet
+  }).toString()
+})
+</script>
+
+<template>
+  <!-- eslint-disable-next-line vue/no-v-html -->
+  <figure v-html="avatar" />
+</template>
--- apps/d.d0rich.me/src/widgets/SidebarMenu.vue
+++ apps/d.d0rich.me/src/widgets/SidebarMenu.vue
@@ -1,7 +1,20 @@
 <script setup lang="ts">
 import TonConnect from '../features/tonconnect/ui/TonConnect.vue'
+import UserAvatar from '../features/user/ui/UserAvatar.vue'
+import { useTonConnectStore } from '@/features/tonconnect/stores/tonConnectStore'
+
+const tonConnectStore = useTonConnectStore()
 </script>
 
 <template>
-  <TonConnect />
+  <div>
+    <section class="flex items-center gap-4">
+      <UserAvatar
+        v-if="tonConnectStore.wallet"
+        class="w-10"
+        :wallet="tonConnectStore.wallet"
+      />
+      <TonConnect />
+    </section>
+  </div>
 </template>
