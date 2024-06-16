0f8694ce9cdd5ca6f5ced8ded44e096ba86d3d16
Nikolai Dorofeev <d0rich.connect@gmail.com>
2024-06-16 11:39:22 +0400

    Add CNAME to CDN

--- cdn/src/main.ts
+++ cdn/src/main.ts
@@ -1,7 +1,11 @@
 import { BrowserStore } from './store/browser'
 import { saveAllResume } from './features/resume/saveAllResume'
+import { dist, publicFolder } from './store/dist'
 
 async function main() {
+  for (const file of await publicFolder.getKeys()) {
+    await dist.setItemRaw(file, await publicFolder.getItemRaw(file))
+  }
   const browser = await BrowserStore.getBrowser()
   await saveAllResume()
   await browser.close()
--- cdn/src/store/dist.ts
+++ cdn/src/store/dist.ts
@@ -4,3 +4,7 @@ import fsDriver from 'unstorage/drivers/fs'
 export const dist = createStorage({
   driver: fsDriver({ base: './dist' })
 })
+
+export const publicFolder = createStorage({
+  driver: fsDriver({ base: './public' })
+})
