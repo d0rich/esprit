f3e043a611ca94fb330e39f198b6146535b8b13c
Nikolai Dorofeev <dorich2000@gmail.com>
2024-09-06 12:13:33 +0400

    Add data-language attribute to code blocks in ProsePre component

---
+++ .changeset/young-dogs-serve.md
@@ -0,0 +1,5 @@
+---
+'@d0rich/nuxt-design-system': patch
+---
+
+Add data-language for code blocks for telegram instant view
---
+++ packages/nuxt-design-system/components/content/ProsePre.vue
@@ -0,0 +1,48 @@
+<template>
+  <ProseCode
+    :code="code"
+    :language="language"
+    :filename="filename"
+    :highlights="highlights"
+    :meta="meta"
+  >
+    <pre
+      :class="$props.class"
+      :style="style"
+      :data-language="language"
+    ><slot /></pre>
+  </ProseCode>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  code: {
+    type: String,
+    default: ''
+  },
+  language: {
+    type: String,
+    default: null
+  },
+  filename: {
+    type: String,
+    default: null
+  },
+  highlights: {
+    type: Array as () => number[],
+    default: () => []
+  },
+  meta: {
+    type: String,
+    default: null
+  },
+  class: {
+    type: String,
+    default: null
+  },
+  style: {
+    type: [String, Object],
+    default: null
+  }
+})
+</script>
