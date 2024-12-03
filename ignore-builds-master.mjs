a283b759b7c82a2cd76257b381b4eadd5c6702d3
Nikolai Dorofeev - d0rich <dorich2000@gmail.com>
2024-12-03 08:44:58 +0400

    Add logging for cached and current commit references

--- ignore-builds-master.mjs
+++ ignore-builds-master.mjs
@@ -6,6 +6,10 @@ const cachedCommitRef =
   process.env.CACHED_COMMIT_REF || 'd06423d365fa9cf3ef99a8653abbf1a83fe7d3bb'
 const commitRef = process.env.COMMIT_REF || 'HEAD'
 
+console.info('Cached commit ref:', cachedCommitRef)
+console.info('Commit ref:', commitRef)
+console.info('Running from: ', process.cwd())
+
 const changesList = execSync(
   `git diff --name-only ${cachedCommitRef} ${commitRef}`
 ).toString()
