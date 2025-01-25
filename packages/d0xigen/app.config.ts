d5887bcbfa823e1c30d86e4c9703730ca58c5534
Nikolai Dorofeev - d0rich <dorich2000@gmail.com>
2025-01-25 08:59:45 +0400

    Refactor d0xigen configuration: extract ThemeConfig to index.d.ts and simplify app.config.ts

--- packages/d0xigen/app.config.ts
+++ packages/d0xigen/app.config.ts
@@ -1,45 +1,3 @@
-import type { RobotsTxtOptions } from './server/routes/robots.txt'
-import type { DeepPartial } from './utils/DeepPartial'
-
-interface ThemeConfig {
-  title: string
-  description: string
-  url: string
-  author: string
-  authorSocial?: {
-    telegram?: `@${string}`
-    website?: string
-    twitter?: `@${string}`
-    twitterSite?: `@${string}`
-  }
-  social?: {
-    github?: string
-  }
-  seo?: {
-    keywords?: string[]
-    robots?: RobotsTxtOptions[]
-  }
-  og: {
-    image: string
-  }
-  features?: {
-    gtag?: {
-      id?: string
-    }
-  }
-  d0richIndex?: {
-    complexity?: number
-    tags: string[]
-    freezeUpdateDate?: Date
-  }
-}
-
-declare module 'nuxt/schema' {
-  interface AppConfigInput {
-    d0xigen?: DeepPartial<ThemeConfig>
-  }
-}
-
 export default defineAppConfig({
   d0xigen: {
     title: 'd0xigen',
@@ -69,7 +27,7 @@ export default defineAppConfig({
     d0richIndex: {
       tags: []
     }
-  } as ThemeConfig,
+  },
   contentMermaid: {
     color: '@nuxtjs/color-mode'
   }
