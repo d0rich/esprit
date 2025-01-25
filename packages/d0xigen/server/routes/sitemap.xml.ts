6f98ea53cc8cebb9d64119a8c54cbe543b5215f7
Nikolai Dorofeev - d0rich <dorich2000@gmail.com>
2025-01-25 12:59:52 +0400

    Remove console log from sitemap.xml.ts

--- packages/d0xigen/server/routes/sitemap.xml.ts
+++ packages/d0xigen/server/routes/sitemap.xml.ts
@@ -3,7 +3,6 @@ import { SitemapStream, streamToPromise } from 'sitemap'
 export default defineEventHandler(async () => {
   // Fetch all documents
   const docs = await queryCollection('content').all()
-  console.log(docs)
   const sitemap = new SitemapStream({
     hostname: useAppConfig().d0xigen.url
   })
