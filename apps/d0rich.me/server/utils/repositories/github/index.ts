1e47e0251a94d2afe310b23ce3526c9918523357
Nikolai Dorofeev - d0rich <dorich2000@gmail.com>
2025-02-16 12:26:19 +0400

    Add public environment variables for GitHub and Netlify tokens

--- apps/d0rich.me/server/utils/repositories/github/index.ts
+++ apps/d0rich.me/server/utils/repositories/github/index.ts
@@ -1,10 +1,8 @@
 import { Octokit } from 'octokit'
 import { GithubRepository as GithubRepositorySource } from './GithubRepository'
 
-console.log('ENVIRONMENT_VARIABLES', process.env)
-
 export const GithubRepository = new GithubRepositorySource(
   new Octokit({
-    auth: process.env.NUXT_PUBLIC_GITHUB_TOKEN
+    auth: process.env.GITHUB_TOKEN || process.env.NUXT_PUBLIC_GITHUB_TOKEN
   })
 )
