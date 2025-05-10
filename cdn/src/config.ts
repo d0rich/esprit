1c35ea29ff8487a48189a66c58e7e38e0c2afc4e
Nikolai Dorofeev - d0rich <dorich2000@gmail.com>
2025-05-10 22:02:01 +0400

    Remove CDN

--- cdn/src/config.ts
+++
@@ -1,3 +0,0 @@
-export const config = {
-  MAIN_BASE_URL: 'https://d0rich.me'
-}
--- cdn/src/models/ResumeList.ts
+++
@@ -1,8 +0,0 @@
-import { z } from 'zod'
-
-export const ResumeSchema = z.object({
-  title: z.string(),
-  path: z.string()
-})
-
-export const ResumeListSchema = z.array(ResumeSchema)
