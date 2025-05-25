a2d41fa0379ac8259919a11db865f545a366ee7d
Nikolai Dorofeev - d0rich <dorich2000@gmail.com>
2025-05-25 17:53:36 +0400

    Remove unused gsap ScrollTrigger plugin

---
+++ .changeset/heavy-insects-relax.md
@@ -0,0 +1,5 @@
+---
+'@d0rich/nuxt-design-system': minor
+---
+
+Remove gsap ScrollTrigger plugin
--- packages/nuxt-design-system/plugins/gsap.client.ts
+++ packages/nuxt-design-system/plugins/gsap.client.ts
@@ -1,10 +1,8 @@
 import { defineNuxtPlugin } from '#imports'
 import gsap from 'gsap'
 import { MotionPathPlugin } from 'gsap/MotionPathPlugin'
-import { ScrollTrigger } from 'gsap/ScrollTrigger'
 
 export default defineNuxtPlugin(() => {
   gsap.registerPlugin(MotionPathPlugin)
-  gsap.registerPlugin(ScrollTrigger)
   window.gsap = gsap
 })
